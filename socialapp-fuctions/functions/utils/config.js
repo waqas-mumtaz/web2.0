00166152e831be5910af957e57583d36e9664547
waqas-mumtaz <42939215+waqas-mumtaz@users.noreply.github.com>
2021-07-11 23:20:35 +0200

    init

---
+++ socialapp-fuctions/functions/utils/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyDKbfwdBfzkN0z-uLm2c_s5mWAPo5rG18E',
+  authDomain: 'socialapp-local.firebaseapp.com',
+  projectId: 'socialapp-local',
+  storageBucket: 'socialapp-local.appspot.com',
+  messagingSenderId: '447711116265',
+  appId: '1:447711116265:web:eb31a3c7959bdd69946978',
+  measurementId: 'G-78E9T1NY95',
+};
